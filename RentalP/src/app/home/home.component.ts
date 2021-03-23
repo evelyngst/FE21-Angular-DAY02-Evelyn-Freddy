65f2e9a7058903db9c9ddb3eae6b6defe171dab1
Freddy <freddy.delgado.porras@gmail.com>
2021-03-23 13:15:20 +0100

    We added the imag and price argument

--- RentalP/src/app/home/home.component.ts
+++ RentalP/src/app/home/home.component.ts
@@ -7,17 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 RentCar = [
-{brand: 'Ford',
+{image: 'https://autosummit.cl/wp-content/uploads/2020/08/FOTO-CATALOGO-ALL-NEW-TERRITORY.png',
+brand: 'Ford',
 color: 'pink',
 doors: 2,
+price: 46,
 },
-{brand: 'Honda',
+{image: 'https://resize.indiatvnews.com/en/resize/oldbucket/715_-/businessindia/Review_Honda_s_3648.jpg',
+brand: 'Honda',
 color: 'green',
 doors: 5,
+price: 39,
 },
-{brand: 'VW',
+{image: 'https://prod.pictures.autoscout24.net/listing-images/6dc584da-24fe-4be8-8ae9-43f1685fce63_f3e12aa9-9cb5-4a45-9001-57e677173034.jpg/640x480.jpg',
+brand: 'VW',
 color: 'Black',
 doors: 4,
+price: 25,
 }
 ];
   constructor() { }
